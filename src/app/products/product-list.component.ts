9fe635c21927c9dab1675fc7f150e341ba8cda0c
Sergey Turinskiy <sergey.turinskiy@dfsco.com>
2018-10-14 17:48:19 +0300

    Add ProductService to retrieve data

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { IOptionsValues, IPerfLoggingPrefs } from 'selenium-webdriver/chrome';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+    styleUrls: ['./product-list.component.css'],
+    providers: [ProductService]
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
@@ -23,32 +24,10 @@ export class ProductListComponent implements OnInit {
 
     filteredProducts: IProduct[];
 
-    products: any[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
+        console.log('In Constructor')
     }
 
     toggleImage(): void {
@@ -57,6 +36,8 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('In OnInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
     performFilter(filterBy: string): IProduct[] {
